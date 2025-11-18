7b7128f33b68fc1ab14ac2078f5fa4d6aae73631
TaoEngine <taoengine@qq.com>
2025-11-18 22:39:55 +0800

    安排了字体

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:m3e_collection/m3e_collection.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -38,10 +39,20 @@ class MainApp extends StatelessWidget {
       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
         return MaterialApp(
           title: "Simuthat",
-          theme: withM3ETheme(ThemeData(fontFamily: "微软雅黑", colorScheme: lightDynamic)),
-          darkTheme: withM3ETheme(ThemeData(fontFamily: "微软雅黑", colorScheme: darkDynamic)),
+          theme: withM3ETheme(ThemeData(fontFamily: "AlibabaPuHuiTi", colorScheme: lightDynamic)),
+          darkTheme: withM3ETheme(ThemeData(fontFamily: "AlibabaPuHuiTi", colorScheme: darkDynamic)),
           themeMode: ThemeMode.system,
-          home: Scaffold(body: Center(child: Text('Hello World!'))),
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 5,
+                children: [Icon(TablerIcons.barrier_block, size: 60), Text('施工中... Construction Area...')],
+              ),
+            ),
+          ),
         );
       },
     );
